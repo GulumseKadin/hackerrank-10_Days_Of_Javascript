8aca47983140d7c18b05976de37f6f24f0871016
serifhanisikli <isikliserifhan@gmail.com>
2022-03-24 08:36:15 +0300

    Day 0

---
+++ Day 0/dataTypes.js
@@ -0,0 +1,11 @@
+function performOperation(secondInteger, secondDecimal, secondString) {
+
+    const _firstInteger = 4;
+    const _firstDecimal = 4.0;
+    const _firstString = 'HackerRank ';
+    
+    console.log(_firstInteger + Number(secondInteger));
+    console.log(_firstDecimal + Number(secondDecimal));    
+    console.log(_firstString.concat(secondString));
+    
+}
